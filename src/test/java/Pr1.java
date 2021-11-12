b85f4b9b913dbca9419a0aceb00d81188230040f
Marika-Kankia <MarikaKankia777@gmail.com>
2021-11-12 22:56:55 +0400

    Fixed details

--- src/test/java/Pr1.java
+++ src/test/java/Pr1.java
@@ -37,37 +37,7 @@ public class Pr1 extends ChromeRunner {
           .CloseButton()
           .EnabledButton();
 
-
-
-
-
-//  $(byXpath("//*[@id=\"myAccountDropdown\"]")).click();
-//  $(byText("Join")).click();
-//  $(By.id("Email")).setValue("marikankia72@gmail.com").click();
-//  $(By.id("FirstName")).setValue("Marika").click();
-//  $(By.id("LastName")).setValue("Kankia").click();
-//  $(By.id("Password")).setValue("marika1357").click();
-//  $(By.id("registration-show-me")).click();
-//  $(By.id("BirthDay")).$(byAttribute("value","24")).click();
-//  $(By.id("BirthMonth")).$(byAttribute("value","11")).click();
-//  $(By.id("BirthYear")).$(byAttribute("value","1999")).click();
-//  $(".tickbox-container",0).click();
-//  $(byText("Select All")).click();
-//  $(By.id("clear-all-checkbox-button")).click();
-//  $(".checkmark",0).waitUntil(Condition.enabled,3000);
-//  $(".checkmark",0).click();
-//  $(".checkmark",0).click();
-//  $(".checkmark",1).click();
-//  $(".checkmark",1).click();
-//  $(".checkmark",2).click();
-//  $(".checkmark",2).click();
-//  $(".checkmark",3).click();
-//  $(".checkmark",3).click();
-//  $(".modal-preference-button-open").click();
-//  $(".close-asos").click();
-//  Assert.assertTrue($(By.id("register")).is(Condition.enabled));
-//  sleep(3000);
-
+  
  }
 
  @Test(retryAnalyzer = Authenticator.Retry.class,priority = 1)
@@ -105,31 +75,6 @@ public class Pr1 extends ChromeRunner {
           .EmptyBirthYear()
           .BirthDayError();
 
-
-
-
-
-
-//  $(byXpath("//*[@id=\"myAccountDropdown\"]")).click();
-//  $(byText("Join")).click();
-//  $(By.id("Email")).setValue("marikakankia").click();
-//  $(By.id("FirstName")).click();
-//  Assert.assertTrue($(By.id("Email-error")).is(Condition.visible));
-//  $(By.id("Email")).setValue("marikakankia72").sendKeys(Keys.chord(Keys.CONTROL,"a", Keys.DELETE));
-//  $(By.id("Email")).setValue("marikakankia72@").click();
-//  $(By.id("FirstName")).click();
-//  Assert.assertTrue($(By.id("Email-error")).is(Condition.visible));
-//  $(By.id("Email")).setValue("marikakankia72@").sendKeys(Keys.chord(Keys.CONTROL,"a", Keys.DELETE));
-//  $(By.id("Email")).setValue("marikakankia72@gmail.com").click();
-//  $(By.id("FirstName")).click();
-//  Assert.assertFalse($(By.id("Email-error")).is(Condition.visible));
-//  $(By.id("LastName")).click();
-//  Assert.assertTrue($(By.id("FirstName-error")).is(Condition.visible));
-//  $(By.id("Password")).click();
-//  Assert.assertTrue($(By.id("LastName-error")).is(Condition.visible));
-//  $(By.id("FirstName")).click();
-//  Assert.assertTrue($(By.id("Password-error")).is(Condition.visible));
-//  Assert.assertTrue($(By.id("register")).is(Condition.disabled));
-
+  
  }
 }
